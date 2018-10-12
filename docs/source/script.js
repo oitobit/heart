15eeaecf2fcf7f3124e3d4684c37952dce70506b
610rodrigo <rodrigo.fla75@gmail.com>
2018-10-12 15:33:34 -0300

    File update

--- docs/source/script.js
+++ docs/source/script.js
@@ -1,5 +1,11 @@
 function myClique () {
     document.getElementById("imgShow").style.display = "block";
     document.getElementById("btnClique").style.display = "none";
+    document.getElementById("hide").style.display = "block";
 }
 
+function back() {
+    document.getElementById("imgShow").style.display = "none";
+    document.getElementById("btnClique").style.display = "block";
+    document.getElementById("hide").style.display = "none";
+}
