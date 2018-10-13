f599a503b30e2b12531e363a0f85a0a74114a834
610rodrigo <rodrigo.fla75@gmail.com>
2018-10-13 13:09:37 -0300

    finished project

--- docs/source/script.js
+++ docs/source/script.js
@@ -2,10 +2,12 @@ function myClique () {
     document.getElementById("imgShow").style.display = "block";
     document.getElementById("btnClique").style.display = "none";
     document.getElementById("hide").style.display = "block";
+    document.getElementById("heartbeat").style.display = "block";
 }
 
 function back() {
     document.getElementById("imgShow").style.display = "none";
     document.getElementById("btnClique").style.display = "block";
     document.getElementById("hide").style.display = "none";
+    document.getElementById("heartbeat").style.display = "none";
 }
